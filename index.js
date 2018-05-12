b613f3345527c060b5a60c00beaeeeaae0c68cb8
Majid Nisar <majidnisar@gmail.com>
2018-05-12 15:00:36 +0530

    :fire: changed the values of the mongodb module to 2.3.31

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 const url = 'mongodb://localhost:27017/conFusion'
-MongoClient.connect(url, (err, client) => {
-  const db = client.db
+MongoClient.connect(url, (err, db) => {
   console.log('trying for connection')
   assert.equal(err, null)
   console.log('Connected correctly to mongo server')
